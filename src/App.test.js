eae057b6f57e3791f637cee0962d123e8e55225c
thiegorechi <rechithiego@gmail.com>
2026-04-08 08:23:13 -0600

    initial react app

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('displays Thiego Gomes Rechi', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(screen.getByText('Thiego Gomes Rechi')).toBeInTheDocument();
 });
